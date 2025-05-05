09c3f05c7b65f24904386c462bba176acfb5f121
NuhaAlfarhani <nuhaalfarhani@student.telkomuniversity.ac.id>
2025-05-05 22:10:05 +0700

    Services get question from JSON and to show the question

---
+++ QuizApp/Models/Question.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace QuizApp.Models;
+
+public class Question
+{
+    public int Id { get; set; }
+    public string Text { get; set; }
+    public List<string> Options { get; set; } = new();
+    public int CorrectIndex { get; set; }
+
+    public bool IsCorrect(int answerIndex) => answerIndex == CorrectIndex;
+}
