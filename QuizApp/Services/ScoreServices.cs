1659524aa773a5163433d11bc25ce458d030e69a
abrielkarisma <101442361+abrielkarisma@users.noreply.github.com>
2025-05-05 23:36:57 +0700

    fix error
    run perfectly

---
+++ QuizApp/Services/ScoreServices.cs
@@ -0,0 +1,30 @@
+using System.Text.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+
+namespace QuizApp.Services
+{
+
+    public static class ScoreService
+    {
+        private static readonly string ScoreFile = "D:/csharp/QuizApp/QuizApp/Data/ScoreLog.json";
+
+        public static void SaveScore(int correct, int total)
+        {
+            var result = new { timestamp = DateTime.Now, correct, total };
+            List<object> allScores = new();
+
+            if (File.Exists(ScoreFile))
+            {
+                var json = File.ReadAllText(ScoreFile);
+                allScores = JsonSerializer.Deserialize<List<object>>(json) ?? new();
+            }
+
+            allScores.Add(result);
+            var updated = JsonSerializer.Serialize(allScores, new JsonSerializerOptions { WriteIndented = true });
+            File.WriteAllText(ScoreFile, updated);
+        }
+    }
+}
